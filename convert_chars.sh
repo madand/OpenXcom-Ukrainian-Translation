7e5388e83a4128529e158a06adfd2cf5b3f21d31
MadAnd <dev.madand@gmail.com>
2012-09-11 03:16:40 +0300

    Додав версію файлу з використанням кирилічних "і"та "ї", а також bash-скрипт для конвертування даних символів в латинські еквіваленти.

---
+++ convert_chars.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ] && [ -z "$2" ]; then
+	echo "Usage: $0 sourceFile desctinationFile" 1>&2
+	exit 1
+fi
+
+cp "$1" "$2"
+perl -p -i -e 's/і/i/g' "$2"
+perl -p -i -e 's/І/I/g' "$2"
+perl -p -i -e 's/ї/ï/g' "$2"
+perl -p -i -e 's/Ї/Ï/g' "$2"
+exit 0;
